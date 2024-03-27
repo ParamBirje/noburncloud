eb958fc0c936b1181349c0abac2821de433cb4c3
Param Birje <parambirje@gmail.com>
2024-03-27 18:49:47 +0530

    Implemented home page

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,6 @@
 import { log } from "@repo/logger";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export const metadata = {
   title: "TheLaunchpad - Startup Simulator",
@@ -9,17 +10,28 @@ export default function Store(): JSX.Element {
   log("Hey! This is the home page.");
 
   return (
-    <main>
-      <h2 className="uppercase text-accent">powered by Gemini</h2>
-      <h1>The startup simulator.</h1>
+    <main className="min-h-screen w-3/4 mx-auto flex justify-center items-center">
+      <div className="text-center flex flex-col gap-8">
+        <div>
+          <h2 className="uppercase text-accent font-bold tracking-wide">
+            powered by Gemini
+          </h2>
+          <h1 className="scroll-m-20 text-4xl font-light tracking-tight lg:text-5xl">
+            Implement cloud architectures <br />
+            in the real world
+          </h1>
+        </div>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque maxime
-        voluptate recusandae doloribus quis laudantium consectetur velit
-        quisquam corporis culpa.
-      </p>
+        <p className="text-muted-foreground tracking-wide w-3/4 mx-auto">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque maxime
+          voluptate recusandae doloribus quis laudantium consectetur velit
+          quisquam corporis culpa.
+        </p>
 
-      <Button>Start up!</Button>
+        <Link href="/onboard">
+          <Button>Architect</Button>
+        </Link>
+      </div>
     </main>
   );
 }
