2b786e9b8d60f4f001be0d0bcaef4be9729bf3c6
Param Birje <parambirje@gmail.com>
2024-03-27 20:43:31 +0530

    Added Deployment section to workbench

--- apps/web/src/app/workbench/page.tsx
+++ apps/web/src/app/workbench/page.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { CircleDollarSign, Heart, Info, Laugh } from "lucide-react";
 import React from "react";
 
@@ -90,6 +97,40 @@ export default function Page() {
             <Button>Add Component</Button>
           </CardFooter>
         </Card>
+      </section>
+
+      <section id="deployment" className="flex flex-col gap-5">
+        <h3 className="font-bold uppercase tracking-wide text-sm">
+          Deployment
+        </h3>
+
+        <div className="flex justify-between gap-5">
+          <Card className="w-full">
+            <CardContent className="py-4">
+              <p className=" text-muted-foreground text-xl font-light">
+                Status <span className="text-accent">Healthy</span>
+              </p>
+            </CardContent>
+          </Card>
+
+          <Card className="w-full">
+            <CardHeader>
+              <CardTitle>Support</CardTitle>
+              <p className="text-sm text-muted-foreground">
+                Chat with the cloud support team here
+              </p>
+            </CardHeader>
+            <CardContent className="py-4">
+              <p className=" text-muted-foreground">some chats here</p>
+            </CardContent>
+            <CardFooter>
+              <div className="flex w-full justify-between items-center space-x-2">
+                <Input type="email" placeholder="Type your message here" />
+                <Button type="submit">Send</Button>
+              </div>
+            </CardFooter>
+          </Card>
+        </div>
       </section>
     </main>
   );
