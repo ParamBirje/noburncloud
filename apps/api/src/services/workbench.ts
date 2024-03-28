6cfd495c6f36a0fa014d38bc78a1a44dec9a0d9b
Param Birje <parambirje@gmail.com>
2024-03-28 18:10:19 +0530

    Added a sample getRequirements() to services

---
+++ apps/api/src/services/workbench.ts
@@ -0,0 +1,14 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+async function getRequirements() {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+  const prompt = "Write a story about a magic backpack.";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  return text;
+}
