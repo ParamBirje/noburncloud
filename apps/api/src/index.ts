20dba9756ecbbb04a5e3ff589cdc80512d758a33
Param Birje <parambirje@gmail.com>
2024-03-28 18:23:57 +0530

    Initialised socket.io in the backend

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,25 @@
+import http from "http";
+import { Server } from "socket.io";
 import { log } from "@repo/logger";
 import { createServer } from "./server";
 
 const port = process.env.PORT || 5001;
-const server = createServer();
+const app = createServer();
+
+// Socket.io setup
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (_req, res) => {
+  return res.json({
+    message: "Hello, this is the backend.",
+  });
+});
+
+io.on("connection", (socket) => {
+  log(`User ${socket.id} has connected!`);
+});
 
 server.listen(port, () => {
-  log(`API running on localhost:${port}`);
+  log(`API running on http://localhost:${port}`);
 });
