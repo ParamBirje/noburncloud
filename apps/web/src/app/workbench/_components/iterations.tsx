5ce4ec72faa5d8561cdb75afca73f0722fde73be
Param Birje <parambirje@gmail.com>
2024-04-02 21:11:57 +0530

    Added dummy cards to iterations

--- apps/web/src/app/workbench/_components/iterations.tsx
+++ apps/web/src/app/workbench/_components/iterations.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import React from "react";
 
 export default function Iterations() {
@@ -7,8 +7,35 @@ export default function Iterations() {
       <h3 className="font-bold uppercase tracking-wide text-sm">Iterations</h3>
 
       <Card>
-        <CardContent className="py-4">
-          <p className=" text-muted-foreground text-sm">All mails caught up!</p>
+        <CardContent className="py-4 flex flex-col gap-4">
+          {/* Subcards */}
+          <Card>
+            <CardHeader>
+              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                Need a database replica to perform analytics
+              </h4>
+            </CardHeader>
+            <CardContent>
+              <p className="text-sm text-muted-foreground">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut
+                facilis ad impedit earum incidunt asperiores.
+              </p>
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader>
+              <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                File storage is needed for user avatars
+              </h4>
+            </CardHeader>
+            <CardContent>
+              <p className="text-sm text-muted-foreground">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut
+                facilis ad impedit earum incidunt asperiores.
+              </p>
+            </CardContent>
+          </Card>
         </CardContent>
       </Card>
     </section>
