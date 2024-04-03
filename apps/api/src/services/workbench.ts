240890b71aca54ef739996f33959fd25df4cf288
Param Birje <parambirje@gmail.com>
2024-04-03 18:10:48 +0530

    Separated oneTimeResponse function from AI

--- apps/api/src/services/workbench.ts
+++ apps/api/src/services/workbench.ts
@@ -2,13 +2,18 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(String(process.env.GEMINI_API));
 
-export async function getRequirements(): Promise<string> {
+async function oneTimeResponse(prompt: string): Promise<string> {
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-  const prompt = "Write a story about a magic backpack.";
-
   const result = await model.generateContent(prompt);
-  const response = await result.response;
+  const response = result.response;
   const text = response.text();
+  return text;
+}
+
+export async function getRequirements(): Promise<string> {
+  const prompt = "Write a story about a magic backpack.";
+  const text = await oneTimeResponse(prompt);
+
   return text;
 }
