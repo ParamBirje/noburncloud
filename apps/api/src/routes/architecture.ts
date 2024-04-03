d77bedc25dca19d2d589ef86a38256b2e9de47ae
Param Birje <parambirje@gmail.com>
2024-04-03 19:53:52 +0530

    Configured api route /architecture, now sends back json architecture components

--- apps/api/src/routes/architecture.ts
+++ apps/api/src/routes/architecture.ts
@@ -1,11 +1,23 @@
 import { Router } from "express";
+import { getArchitectureComponents } from "../services/workbench";
+import { log } from "@repo/logger";
 
 const router = Router();
 
 router.put("/", async (req, res) => {
-  return res.json({
-    text: "",
-  });
+  const archDesc = req.body.desc;
+
+  try {
+    const components = await getArchitectureComponents(archDesc);
+    return res.json({
+      components: JSON.parse(components),
+    });
+  } catch (e) {
+    log(`Error: Processing architecture components\n\t`);
+    return res.json({
+      error: "Cannot process json.",
+    });
+  }
 });
 
 export default router;
