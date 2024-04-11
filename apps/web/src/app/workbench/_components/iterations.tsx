87e08703ea24f0c13d8f4a4d1820a649eb54235d
Param Birje <parambirje@gmail.com>
2024-04-11 18:50:39 +0530

    Added scroll area to wrap iterations

--- apps/web/src/app/workbench/_components/iterations.tsx
+++ apps/web/src/app/workbench/_components/iterations.tsx
@@ -4,9 +4,9 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useAtom } from "jotai";
 import React from "react";
 import { iterationAtom } from "../page";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Iterations() {
-  // iterationAtom
   const [iteration] = useAtom(iterationAtom);
 
   return (
@@ -14,22 +14,26 @@ export default function Iterations() {
       <h3 className="font-bold uppercase tracking-wide text-sm">Iterations</h3>
 
       <Card>
-        <CardContent className="py-4 flex flex-col gap-4">
-          {/* Subcards */}
-          {iteration.map((iter: Iteration, index: number) => (
-            <Card key={index}>
-              <CardHeader>
-                <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
-                  {iter.title}
-                </h4>
-              </CardHeader>
-              <CardContent>
-                <p className="text-sm text-muted-foreground">
-                  {iter.description}
-                </p>
-              </CardContent>
-            </Card>
-          ))}
+        <CardContent>
+          <ScrollArea className="h-[50vh] w-full">
+            <div className="py-4 flex flex-col-reverse gap-4">
+              {/* Subcards */}
+              {iteration.map((iter: Iteration, index: number) => (
+                <Card key={index}>
+                  <CardHeader>
+                    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+                      {iter.title}
+                    </h4>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="text-sm text-muted-foreground">
+                      {iter.description}
+                    </p>
+                  </CardContent>
+                </Card>
+              ))}
+            </div>
+          </ScrollArea>
         </CardContent>
       </Card>
     </section>
