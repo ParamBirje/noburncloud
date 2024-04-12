884d119e3b28417e80517a24bc69c5d575eabe3f
Param Birje <parambirje@gmail.com>
2024-04-12 19:44:44 +0530

    Extracted socket listeners into SocketProvider component

--- apps/web/src/app/workbench/page.tsx
+++ apps/web/src/app/workbench/page.tsx
@@ -7,6 +7,7 @@ import Architecture from "./_components/architecture/architecture";
 import Iterations from "./_components/iterations";
 import Notifications from "./_components/notifications";
 import ChatSupport from "./_components/hosting-support/support";
+import SocketProvider from "@/components/socket-provider";
 
 // states
 export const requirementsAtom = atom("");
@@ -20,39 +21,41 @@ export const notificationsAtom = atom<string[]>([]);
 
 export default function Page() {
   return (
-    <main className="my-10 w-3/4 mx-auto flex flex-col gap-16 relative">
-      <div className="flex justify-between items-center">
-        {/* Socket handler here */}
-        <Stats />
+    <SocketProvider>
+      <main className="my-10 w-3/4 mx-auto flex flex-col gap-16 relative">
+        <div className="flex justify-between items-center">
+          {/* Socket handler here */}
+          <Stats />
 
-        <div className="flex flex-col items-end gap-5">
-          <div className="text-right">
-            <h1 className="scroll-m-20 text-3xl font-light tracking-tight lg:text-4xl">
-              The Workbench<span className="text-accent">.</span>
-            </h1>
+          <div className="flex flex-col items-end gap-5">
+            <div className="text-right">
+              <h1 className="scroll-m-20 text-3xl font-light tracking-tight lg:text-4xl">
+                The Workbench<span className="text-accent">.</span>
+              </h1>
 
-            <p className="text-muted-foreground tracking-wide">
-              All the tools you need, in one place.
-            </p>
+              <p className="text-muted-foreground tracking-wide">
+                All the tools you need, in one place.
+              </p>
+            </div>
+
+            <Button className="flex items-center gap-2" variant="secondary">
+              <Info size={20} />
+              <p>Help</p>
+            </Button>
           </div>
-
-          <Button className="flex items-center gap-2" variant="secondary">
-            <Info size={20} />
-            <p>Help</p>
-          </Button>
         </div>
-      </div>
 
-      <Requirements />
+        <Requirements />
 
-      <Architecture />
+        <Architecture />
 
-      <ChatSupport />
+        <ChatSupport />
 
-      <Iterations />
+        <Iterations />
 
-      {/* Docked to the right */}
-      <Notifications />
-    </main>
+        {/* Docked to the right */}
+        <Notifications />
+      </main>
+    </SocketProvider>
   );
 }
---
+++ apps/web/src/components/socket-provider.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import {
+  architectureAtom,
+  iterationAtom,
+  notificationsAtom,
+  requirementsAtom,
+} from "@/app/workbench/page";
+import { log } from "@repo/logger";
+import { useAtom } from "jotai";
+import { useEffect, useRef } from "react";
+import { Socket, io } from "socket.io-client";
+
+export default function SocketProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [requirements] = useAtom(requirementsAtom);
+  const [architecture] = useAtom(architectureAtom);
+  const [iteration, setIteration] = useAtom(iterationAtom);
+  const [notifications, setNotifications] = useAtom(notificationsAtom);
+  const socketRef = useRef<Socket | null>(null);
+
+  useEffect(() => {
+    socketRef.current = io("http://localhost:5001");
+
+    return () => {
+      socketRef.current?.close();
+    };
+  }, []);
+
+  // For emitting the stats to the server
+  useEffect(() => {
+    if (requirements !== "" && architecture.prompt !== "") {
+      socketRef.current?.on("give-stats", () => {
+        socketRef.current?.emit("update", {
+          requirement: requirements,
+          architectureDescription: architecture.prompt,
+        });
+      });
+    } else {
+      log("Requirements or architecture is empty");
+    }
+
+    return () => {
+      socketRef.current?.off("give-stats");
+    };
+  }, [requirements, architecture]);
+
+  // For receiving the new iteration
+  useEffect(() => {
+    socketRef.current?.on("new-iteration", (data) => {
+      setIteration([...iteration, data]);
+    });
+
+    return () => {
+      socketRef.current?.off("new-iteration");
+    };
+  });
+
+  // For emitting the architecture description to the server
+  useEffect(() => {
+    if (architecture.prompt !== "") {
+      socketRef.current?.on("give-architecture", () => {
+        socketRef.current?.emit("update-architecture", {
+          architectureDescription: architecture.prompt,
+        });
+      });
+    } else {
+      log("Architecture is empty");
+    }
+
+    return () => {
+      socketRef.current?.off("give-architecture");
+    };
+  }, [architecture]);
+
+  // For receiving the new notification
+  useEffect(() => {
+    socketRef.current?.on("new-notification", (data) => {
+      setNotifications([...notifications, data]);
+    });
+
+    return () => {
+      socketRef.current?.off("new-notification");
+    };
+  });
+
+  return <>{children}</>;
+}
