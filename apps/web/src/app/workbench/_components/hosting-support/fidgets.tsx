0ab106e6ea2c42a24b0b53f6fa5599b09afbccfa
Param Birje <parambirje@gmail.com>
2024-04-19 19:35:26 +0530

    Added placeholder text, ui fixes on fidget components in hosting section

--- apps/web/src/app/workbench/_components/hosting-support/fidgets.tsx
+++ apps/web/src/app/workbench/_components/hosting-support/fidgets.tsx
@@ -28,42 +28,40 @@ export default function Fidgets() {
     };
   }, []);
 
-  return (
-    architecture.components.length > 0 && (
-      <>
-        <p className=" text-muted-foreground text-xl font-light">
-          Status <span className="text-accent">Active</span>
-        </p>
+  return architecture.components.length === 0 ? (
+    <p className="text-sm text-muted-foreground">
+      Your deployment dashboard will appear here.
+    </p>
+  ) : (
+    <>
+      <p className=" text-muted-foreground text-xl font-light">
+        Status <span className="text-accent">Active</span>
+      </p>
 
-        <Card className="flex items-center justify-between px-4 py-3">
-          <p className="text-muted-foreground text-lg font-light">
-            Current Users
-          </p>
-          <Progress className="w-[60%] h-3" value={progress.users} />
-        </Card>
+      <Card className="flex items-center justify-between px-4 py-3">
+        <p className="text-muted-foreground">Current Users</p>
+        <Progress className="w-[60%] h-3" value={progress.users} />
+      </Card>
 
-        <p className=" text-muted-foreground text-xl font-light mt-5">
-          Services <span className="text-yellow-400">Usage</span>
-        </p>
+      <p className=" text-muted-foreground text-xl font-light mt-5">
+        Services <span className="text-yellow-400">Usage</span>
+      </p>
 
-        {architecture.components
-          .sort(() => Math.random() - 0.5)
-          .slice(0, 3)
-          .map((component: any) => (
-            <Card
-              className="flex items-center justify-between gap-5 px-4 py-3"
-              key={component.name}
-            >
-              <p className="text-muted-foreground font-light">
-                {component.name}
-              </p>
-              <Progress
-                className="w-1/3 h-3"
-                value={Math.floor(Math.random() * 100) + 20}
-              />
-            </Card>
-          ))}
-      </>
-    )
+      {architecture.components
+        .sort(() => Math.random() - 0.5)
+        .slice(0, 3)
+        .map((component: any) => (
+          <Card
+            className="flex items-center justify-between gap-5 px-4 py-3"
+            key={component.name}
+          >
+            <p className="text-muted-foreground">{component.name}</p>
+            <Progress
+              className="w-1/3 h-3"
+              value={Math.floor(Math.random() * 100) + 20}
+            />
+          </Card>
+        ))}
+    </>
   );
 }
